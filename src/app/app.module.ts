7d62ba65c0c4d0552b3b57f6dc5076e10d454609
TrongDuongIT <nguyentrongduongit@gmail.com>
2024-03-23 20:30:09 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BioComponentComponent } from './components/bio-component/bio-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/bio-component/bio-component.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bio-component',
+  templateUrl: './bio-component.component.html',
+  styleUrls: ['./bio-component.component.scss']
+})
+export class BioComponentComponent implements OnInit {
+
+  public isDisplay: boolean = false;
+  public title: string = "";
+  public image: string = "";
+  public list: any = [
+    { id: 1, name: "Panorama 01", description: "Panorama 01", link: "../assets/images/panorama01.jpg", image:"../assets/images/panorama01.jpg" },
+    { id: 2, name: "Panorama 02", description: "Panorama 02", link: "../assets/images/panorama02.jpg", image:"../assets/images/panorama02.jpg" },
+    { id: 3, name: "Panorama 03", description: "Panorama 03", link: "../assets/images/panorama03.jpg", image:"../assets/images/panorama03.jpg" },
+    { id: 4, name: "Panorama 04", description: "Panorama 04", link: "../assets/images/panorama04.jpg", image:"../assets/images/panorama04.jpg" },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public handleClick(eventName: string) {
+    switch(eventName) {
+      case 'facebook':
+        window.open("https://www.facebook.com/NguyenTrongDuong.CaptainNui");
+        break;
+      case 'tiktok':
+        window.open("https://www.tiktok.com/@duongnguyenreviewer");
+        break;
+      default:
+        break;
+    }
+  }
+
+  public handleShowDialog(item: any) {
+    this.isDisplay = true;
+    console.log('item.name', item.name);
+    console.log('item.image', item.image);
+    
+    this.title = item.name;
+    this.image = item.image;
+  }
+
+  public handleCloseDialog(isDisplay: boolean) {
+    this.isDisplay = isDisplay;
+  }
+
+}
